4118d8ec09a2d7ea4cccd5849a26dff406fa102a
huynvtasc <huynv@tasc.vn>
2024-07-18 06:40:29 +0700

    add doc for lab

--- 20240715_hibernate/SpringDataJpaWithCache/src/main/java/org/ngvhuy/SpringDataJpaWithCache/repository/DatasetRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ngvhuy.SpringDataJpaWithCache.repository;
-
-import org.ngvhuy.SpringDataJpaWithCache.entity.Dataset;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DatasetRepository extends JpaRepository<Dataset, Long> {
-}
--- 20240715_hibernate/SpringDataJpaWithCache/src/main/java/org/ngvhuy/SpringDataJpaWithCache/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ngvhuy.SpringDataJpaWithCache.repository;
-
-import org.ngvhuy.SpringDataJpaWithCache.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String name);
-}
